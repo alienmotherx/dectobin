04d23e2039a63c43d0526d13201cf01d9444cd45
alienmotherx <45299452+alienmotherx@users.noreply.github.com>
2020-03-19 15:57:59 +0100

    Add files via upload

---
+++ dectobin.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Decimal to binary converter
+	var input int
+	fmt.Println("Input Integer: ")
+	fmt.Scan(&input)
+
+	fmt.Printf("%b", input)
+
+}
